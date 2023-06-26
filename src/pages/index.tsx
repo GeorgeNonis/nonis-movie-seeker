8ccbde83cb48846a319ece2a00fdd1fccb9b0473
GeorgeNonis <georgenonis@gmail.com>
2023-06-26 22:29:04 +0200

    moved the movies into a json file so I can also implement search feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
 import axios from "axios";
+// import { writeFile } from "fs/promises";
+// import { cwd } from "process";
+// import { writeFileSync } from "fs";
+// import path from "path";
+import data from "../../data.json";
 import Starsbackdrop from "@/components/starsbackdrop";
 import styles from "../styles/Home.module.css";
 import Headercomponent from "@/components/headercomponent";
@@ -11,7 +16,6 @@ interface HomePageProps {
   movies: Movie[];
   twentymovies: Movie[];
 }
-
 const HomePage = ({ ...rest }: HomePageProps) => {
   return (
     <>
@@ -26,35 +30,41 @@ const HomePage = ({ ...rest }: HomePageProps) => {
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const axios = require("axios");
-
-  const options = {
-    method: "GET",
-    url: "https://imdb-top-100-movies1.p.rapidapi.com/",
-    headers: {
-      "X-RapidAPI-Key": "8769a608cdmsh896514e83b6b8f6p1f0d3ejsnd53d3144dd92",
-      "X-RapidAPI-Host": "imdb-top-100-movies1.p.rapidapi.com",
-    },
-  };
-  let movies: any | Movie[] = [];
+  // console.log({ data });
+  // const axios = require("axios");
+  // const options = {
+  //   method: "GET",
+  //   url: "https://imdb-top-100-movies.p.rapidapi.com/",
+  //   headers: {
+  //     "X-RapidAPI-Key": "8769a608cdmsh896514e83b6b8f6p1f0d3ejsnd53d3144dd92",
+  //     "X-RapidAPI-Host": "imdb-top-100-movies1.p.rapidapi.com",
+  //   },
+  // };
+  // let movies: any | Movie[] = [];
   let twentymovies: any | Movie[] = [];
 
-  try {
-    const response = await axios.request(options);
-    movies = [...response.data];
-
-    let i = 0;
-    while (i < 20) {
-      twentymovies.push(movies[i]);
-      i++;
-    }
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+  let i = 0;
+  while (i < 20) {
+    twentymovies.push(data[i]);
+    i++;
   }
+
+  // try {
+  //   const response = await axios.request(options);
+  //   movies = [...response.data];
+  //   // writeFile(filePath, movies);
+  //   // console.log(response.data);
+  //   let i = 0;
+  //   while (i < 20) {
+  //     twentymovies.push(movies[i]);
+  //     i++;
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
   return {
     props: {
-      movies: [...movies],
+      movies: [...data],
       twentymovies: [...twentymovies],
     },
     revalidate: 30,
