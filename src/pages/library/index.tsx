4fe2a86b1f25feed6bf42277ac2f72f03fd43c33
GeorgeNonis <georgenonis@gmail.com>
2023-07-13 17:06:18 +0200

    added getStaticProps on Library index.tsx page

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -3,4 +3,11 @@ import { LibraryMovies } from "@/components/modals/myLibrary/options";
 const Library = () => {
   return <LibraryMovies />;
 };
+
+export const getStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 60,
+  };
+};
 export default Library;
